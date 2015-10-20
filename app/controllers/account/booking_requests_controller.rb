e3fffb6410f69b63e419b23a44dc3bed85dca934
Moul <antoine.mouliere@gmail.com>
2015-10-20 15:29:09 +0200

    Add accept/reject

--- app/controllers/account/booking_requests_controller.rb
+++ app/controllers/account/booking_requests_controller.rb
@@ -3,5 +3,19 @@ module Account
     def index
       @booking_requests = BookingRequest.joins(:flat).where(flats: { owner_id: current_user.id })
     end
+
+    def update
+      @booking_request = BookingRequest.find(params[:id])
+      @booking_request.update(status: params[:booking_request][:status])
+
+      if @booking_request.valid?
+        flash[:notice] = "The booking request has been #{params[:booking_request][:status]}"
+        @booking_request.save
+      else
+        flash[:alert] = "An error occured, please try again."
+      end
+
+      redirect_to account_booking_requests_path
+    end
   end
 end
