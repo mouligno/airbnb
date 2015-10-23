4c82b8a733295734457204cab8ecc4761c7bb97d
Moul <antoine.mouliere@gmail.com>
2015-10-23 15:34:53 +0200

    Add all markers on hp

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,12 +1,14 @@
 class FlatsController < ApplicationController
   def index
     if params[:search]
-      @flats = Flat.near("#{params[:search]}", 10).page params[:page]
+      @flats_marker = Flat.near("#{params[:search]}", 10)
+      @flats        = Flat.near("#{params[:search]}", 10).page params[:page]
     else
+      @flats_marker = Flat.all
       @flats = Flat.all.page params[:page]
     end
 
-    @markers = Gmaps4rails.build_markers(@flats) do |flat, marker|
+    @markers = Gmaps4rails.build_markers(@flats_marker) do |flat, marker|
       marker.lat        flat.latitude
       marker.lng        flat.longitude
       marker.infowindow render_to_string(partial: 'map_flat_box', locals: {flat: flat})
